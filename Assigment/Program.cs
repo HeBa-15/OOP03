c254a749ecfba4a23ff60a655e4cf7c5326c9bd7
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-13 08:10:30 +0200

    Part01 Q03

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -1,5 +1,34 @@
 ﻿namespace Assigment
 {
+    #region Q03
+
+    public class ComplexNumber
+    {
+
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+
+        public static ComplexNumber operator +(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber
+            {
+                Real = (left?.Real ?? 0) + (right?.Real ?? 0),
+                Imag = (left?.Imag ?? 0) + (right?.Imag ?? 0),
+            };
+
+        }
+
+        public static ComplexNumber operator -(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber
+            {
+                Real = (left?.Real ?? 0) - (right?.Real ?? 0),
+                Imag = (left?.Imag ?? 0) - (right?.Imag ?? 0),
+            };
+        }
+    }
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
