32691ef9bb6bd609db480d4128e4a6cf079ae2c2
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-13 18:52:11 +0200

    Part01 Q5

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assigment
+﻿using System;
+
+namespace Assigment
 {
     #region Q03
 
@@ -29,11 +31,58 @@
         }
     }
     #endregion
+
+
+    #region Q05
+
+    public class BaseClass
+    {
+      
+        public virtual void DisplayMessage()
+        {
+            Console.WriteLine("Message from BaseClass");
+        }
+    }
+
+    public class DerivedClass1 : BaseClass
+    {
+      
+        public override void DisplayMessage()
+        {
+            Console.WriteLine("Message from DerivedClass1");
+        }
+    }
+
+    public class DerivedClass2 : BaseClass
+    {
+        
+        public new void DisplayMessage()
+        {
+            Console.WriteLine("Message from DerivedClass2");
+        }
+    }
+
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q05
+
+            BaseClass Base;
+            DerivedClass1 derived1 = new DerivedClass1();
+            DerivedClass2 derived2 = new DerivedClass2();
+
+            Base = derived1;
+            Base.DisplayMessage(); // Message from DerivedClass1
+
+            Base = derived2;
+            Base.DisplayMessage(); // Message from BaseClass
+
+            derived2.DisplayMessage(); // Message from DerivedClass2
+
+            #endregion
         }
     }
 }
